dac378cca91a00d1a109e24c3d62c26021602906
bramvelthoven <73891044+bramvelthoven@users.noreply.github.com>
2021-09-08 10:36:05 +0200

    Eerste test

---
+++ script.js
@@ -0,0 +1,28 @@
+function login()
+	{
+		var uname = document.getElementById("email").value;
+		var pwd = document.getElementById("pwd").value;
+		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		if(uname =='')
+		{
+			alert("Voer gebruiksernaam in.");
+		}
+		else if(pwd=='')
+		{
+        	alert("Voer wachtwoord in.");
+		}
+		else if(!filter.test(uname))
+		{
+			alert("Gebruik een geldige email.");
+		}
+		else if(pwd.length < 2 || pwd.length > 10)
+		{
+			alert("Wachtwoord moet tussen de 2 en 10 tekens bevatten.");
+		}
+		else
+		{
+	alert('U bent ingelogd!');
+  
+}
+    }
+	
